4492e88456c1df5b4e9160b5390bb6f6079bceb2
Muhammad Anas <muhammadanasasif3@gmail.com>
2023-02-17 09:41:12 +0500

    JAvascript Array

---
+++ app.js
@@ -0,0 +1,24 @@
+var subjects = ["Chemistry","Maths","English","Urdu","Islamiyat","Organic"]
+console.log(subjects.length)
+
+// Removing from start
+
+subjects.shift()
+console.log(subjects)
+var checkingRemovingFromStart = subjects.shift() 
+// Adding in start
+
+subjects.unshift("DSA")
+console.log(subjects)
+
+// // Adding in end 
+
+subjects.push("A.I","Numerical Computting")
+console.log(subjects)
+
+// // Removing in end
+
+subjects.pop()
+console.log(subjects)
+
+
